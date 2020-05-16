9ec340971a86f9ba7b813b6a0d9eae3e291b405e
skfirozz <shaikfiroz838@gmail.com>
2020-05-16 12:12:14 +0530

    [skfirozz]Refactor.modification in TicTacToe

--- BaicPrograms/TicTacToy.java
+++ BaicPrograms/TicTacToy.java
@@ -3,38 +3,59 @@ import java.util.Scanner;
 
 public class TicTacToy {
 
-	static int c=0;
 	static Random r=new Random();
 	static Scanner scan=new Scanner(System.in);
 	static boolean WF=false;
 	public static int array[][]=new int[3][3];
-	public static void inputss()
+	public static void systemInputs()
 	{
+		int i=r.nextInt(3);
+		int j=r.nextInt(3);
+		if(array[i][j]==1||array[i][j]==2)
+			systemInputs();
+		else 
+			array[i][j]=1;
 	}
-	static void user()
+	static void userInputs()
 	{
-		System.out.print("-->");
+		System.out.println("enter a row:  ");
 		int row=scan.nextInt();
+		System.out.println("enter a column: ");
 		int col=scan.nextInt();
 		if(array[row][col]==1 || array[row][col]==2)
 		{
 			System.out.println("position already occupied");
-			user();
+			userInputs();
 		}
 		else
 		{
 			array[row][col]=2;
 		}
 	}
-	public static boolean checkingWinOrLose(int array[][])
+	public static boolean checkingWinOrLose(int arr[][],int n)
 	{
-		if(array[0][0] == array[0][1])
-		return true;
-		else
-		return false;
-		
+		   if (
+		            arr[0][0] == n && arr[0][1] == n && arr[0][2] == n || arr[1][0] == n && arr[1][1] == n && arr[1][2] == n
+		         || arr[2][0] == n && arr[2][1] == n && arr[2][2] == n || arr[0][0] == n && arr[1][0] == n && arr[2][0] == n
+		         || arr[0][1] == n && arr[1][1] == n && arr[2][1] == n || arr[0][2] == n && arr[1][2] == n && arr[2][2] == n
+		         || arr[0][0] == n && arr[1][1] == n && arr[2][2] == n || arr[0][2] == n && arr[1][1] == n && arr[2][0] == n
+		        )
+		            return true;
+		   else
+			   return false;
 	}
 	public static void main(String[] args) {
 	
+		for(int i=1;i<5;i++)
+		{
+			if(i==9)
+				break;
+			userInputs();
+			if(checkingWinOrLose(array, 2))
+				System.out.println("you won");
+			systemInputs();
+			if(checkingWinOrLose(array, 1))
+				System.out.println("System won");
+		}
 	}
 }
